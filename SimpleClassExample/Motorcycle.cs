58c9ffa225129feb2041a28493bcb036c8d5bcfb
Tigris <tabula.tenebris@gmail.com>
2021-09-13 19:57:37 +0600

    Add project files.

---
+++ SimpleClassExample/Motorcycle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Motorcycle
+    {
+        public int driverIntensity;
+        public string name;
+        
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("Yeee Haaaeeewww!");
+            }
+        }
+        //public Motorcycle() { }
+        //public Motorcycle(int intensity) : this(intensity, "") { }
+        //public Motorcycle(string name) : this(0, name) { }
+        //public Motorcycle(int intensivity, string name)
+        //{
+        //    if (intensivity>10)
+        //    {
+        //        intensivity = 0;
+        //    }
+        //    driverIntensity = intensivity;
+        //    this.name = name;
+        //}
+        public Motorcycle(int intensivity=0, string name="")
+        {
+            if (intensivity > 10)
+            {
+                intensivity = 10;
+            }
+            driverIntensity = intensivity;
+            this.name = name;
+        }
+
+        public void SetDriverName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car chuck = new Car();
+            chuck.PrintState();
+
+            Car mary = new Car("Mary");
+            mary.PrintState();
+
+            Car daisy = new Car(cs: 75, pn: "Daisy");
+            daisy.PrintState();
+
+            Motorcycle mc = new Motorcycle(15);
+            mc.PopAWheely();
+            Console.WriteLine();
+
+            Motorcycle c = new Motorcycle();
+            c.SetDriverName("Tiny");
+            c.PopAWheely();
+            Console.WriteLine($"Rider's name is {c.name}"); ;
+            Console.WriteLine();
+        }
+    }
+}
